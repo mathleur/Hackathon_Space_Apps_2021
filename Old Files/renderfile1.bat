ec5086b4d486c7c10a0c5b3bc750fa11fc1bd68d
tooprose543 <tooprose@outlook.com>
2021-10-01 19:16:18 +0100

    File Restructuring

---
+++ Old Files/renderfile1.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:\Program Files\Blender Foundation\Blender 2.93\blender.exe" --background blank.blend --python "C:\Users\Mathilde\OneDrive\Dokumente\hackathon2021\BlenderAsteroid.py" 
+Echo Rendered all!
+pause
--- renderfile1.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-"C:\Program Files\Blender Foundation\Blender 2.93\blender.exe" --background blank.blend --python "C:\Users\Mathilde\OneDrive\Dokumente\hackathon2021\BlenderAsteroid.py" 
-Echo Rendered all!
-pause
