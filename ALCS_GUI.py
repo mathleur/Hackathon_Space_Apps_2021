b26278aaee93c114ba5300dbc9d41ed5773e0ecc
tooprose543 <tooprose@outlook.com>
2021-10-02 15:38:08 +0100

    Update ALCS_GUI.py

--- ALCS_GUI.py
+++ ALCS_GUI.py
@@ -19,23 +19,15 @@ labelLeft.grid(row = 0, column = 0)
 labelRight = Label(master = frameRight, text = "Right")
 labelRight.grid(row = 0, column = 0)
 
-x=np.array ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-v= np.array ([16,16.31925,17.6394,16.003,17.2861,17.3131,19.1259,18.9694,22.0003,22.81226])
-p= np.array ([16.23697,     17.31653,     17.22094,     17.68631,     17.73641 ,    18.6368, 19.32125,     19.31756 ,    21.20247  ,   22.41444   ,  22.11718  ,   22.12453])
+#x=np.array ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+#v= np.array ([16,16.31925,17.6394,16.003,17.2861,17.3131,19.1259,18.9694,22.0003,22.81226])
+#p= np.array ([16.23697,     17.31653,     17.22094,     17.68631,     17.73641 ,    18.6368, 19.32125,     19.31756 ,    21.20247  ,   22.41444   ,  22.11718  ,   22.12453])
 
 fig = Figure(figsize=(6,6))
 a = fig.add_subplot(111)
-a.scatter(v,x,color='red')
-a.plot(p, range(2 +max(x)),color='blue')
-a.invert_yaxis()
-
-a.set_title ("Test Plot", fontsize=16)
-a.set_ylabel("Y", fontsize=14)
-a.set_xlabel("X", fontsize=14)
-
-canvas = FigureCanvasTkAgg(fig, master=frameRight)
-canvas.get_tk_widget().grid(row = 1, column = 0)
-
+#a.scatter(v,x,color='red')
+#a.plot(p, range(2 +max(x)),color='blue')
+#a.invert_yaxis()
 
 
 #Light Curve
@@ -49,6 +41,17 @@ for i in range(360):
 brightnesses = np.asarray(brightnesses)
 
 x = range(360)
+x = np.array(x)
+brightnesses = np.array(brightnesses)
+a.plot(x,brightnesses)
+
+a.set_title ("Test Plot", fontsize=16)
+a.set_ylabel("Y", fontsize=14)
+a.set_xlabel("X", fontsize=14)
+
+canvas = FigureCanvasTkAgg(fig, master=frameRight)
+canvas.get_tk_widget().grid(row = 1, column = 0)
+
 
 #plt.plot(x, brightnesses)
 #plt.savefig('E:\\Users\\John\\OneDrive\\Hackathon\\GithubFiles\\Space-Apps-Hackathon\\lightcurve.png')
